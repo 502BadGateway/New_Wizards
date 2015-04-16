ebe48d0034d66010758422a4e8c2d392d5feb4ef
Sam Barratt <milktheelephant@gmail.com>
2015-04-16 15:22:27 +0100

    Fixed some indenting issues

--- city.py
+++ city.py
@@ -2,20 +2,22 @@ from gen_arena import arena
 
 class city():
 
-	def __init__(self, cityName, treasuresList):
-		
-		self.__name = cityName 
-		self.arena = arena(self.__name, treasuresList)   #Create a new instance of an arena in the city class.
-		self.__image = self.arena.ret_image_path() 
-                print "City setup finished"
+    def __init__(self, cityName, treasuresList, gen=False, image=""):
+        self.__name = cityName 
+        if gen == True:
+            self.arena = arena(self.__name, treasuresList, image, False, False)
+        else:
+            self.arena = arena(self.__name, treasuresList)   #Create a new instance of an arena in the city class.
+            self.__image = self.arena.ret_image_path() 
+        print "City setup finished"
 
 
-        def ret_image_path(self):
-            print self.__image
-            return self.__image
+    def ret_image_path(self):
+        print self.__image
+        return self.__image
 
-        def retArena(self):
-            return self.arena
+    def retArena(self):
+        return self.arena
 
-        def retArenaList(self):
-            return self.arena.full_arena
+    def retArenaList(self):
+        return self.arena.full_arena
